b846ef807fa1716c4a3161459a55251d1864001a
Allan <xuguangliangu01@126.com>
2019-02-11 16:37:57 +0800

    注释掉了异常管理类

--- Pro/C/U3D/TSG/Assets/Scripts/Manager/Exception/CSExceptionManager.cs
+++ Pro/C/U3D/TSG/Assets/Scripts/Manager/Exception/CSExceptionManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CSExceptionManager : CSMonoSingleton<CSExceptionManager>
 {
@@ -16,15 +13,16 @@ public class CSExceptionManager : CSMonoSingleton<CSExceptionManager>
     public override void Init()
     {
         base.Init();
-        return;
-        LogPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/"));
-        BugExePath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "\\Bug.exe";
+        Debug.Log("异常管理类初始化");
+        //    return;
+        //    LogPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/"));
+        //    BugExePath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/")) + "\\Bug.exe";
 
-        if (IsHandler)
-        {
-            Application.logMessageReceived -= Handler;
-            Application.logMessageReceived += Handler;
-        }
+        //    if (IsHandler)
+        //    {
+        //        Application.logMessageReceived -= Handler;
+        //        Application.logMessageReceived += Handler;
+        //    }
     }
     private void OnDestroy()
     {
@@ -32,42 +30,42 @@ public class CSExceptionManager : CSMonoSingleton<CSExceptionManager>
     }
     private void Handler(string condition, string stackTrace, LogType type)
     {
-        string logPath = "";
-        switch (type)
-        {
-            case LogType.Error:
-            case LogType.Assert:
-            case LogType.Exception:
-                logPath = LogPath + "\\" + DateTime.Now.ToString("yyyy_MM_dd HH_mm_ss") + ".log";
-                break;
-            case LogType.Log:
-                break;
-            case LogType.Warning:
-                break;
-            default:
-                break;
-        }
-        //打印日志
-        if (Directory.Exists(LogPath))
-        {
-            File.AppendAllText(logPath, "[time]:" + DateTime.Now.ToString() + "\r\n");
-            File.AppendAllText(logPath, "[type]:" + type.ToString() + "\r\n");
-            File.AppendAllText(logPath, "[exception message]:" + condition + "\r\n"); File.AppendAllText(logPath, "[stack trace]:" + stackTrace + "\r\n");
-        }
-        //启动bug反馈程序
-        if (File.Exists(BugExePath))
-        {
-            ProcessStartInfo pros = new ProcessStartInfo();
-            pros.FileName = BugExePath;
-            pros.Arguments = "\"" + logPath + "\"";
-            Process pro = new Process();
-            pro.StartInfo = pros; pro.Start();
-        }
-        //退出程序，bug反馈程序重启主程序
-        if (IsQuitWhenException)
-        {
-            Application.Quit();
-        }
+        //string logPath = "";
+        //switch (type)
+        //{
+        //    case LogType.Error:
+        //    case LogType.Assert:
+        //    case LogType.Exception:
+        //        logPath = LogPath + "\\" + DateTime.Now.ToString("yyyy_MM_dd HH_mm_ss") + ".log";
+        //        break;
+        //    case LogType.Log:
+        //        break;
+        //    case LogType.Warning:
+        //        break;
+        //    default:
+        //        break;
+        //}
+        ////打印日志
+        //if (Directory.Exists(LogPath))
+        //{
+        //    File.AppendAllText(logPath, "[time]:" + DateTime.Now.ToString() + "\r\n");
+        //    File.AppendAllText(logPath, "[type]:" + type.ToString() + "\r\n");
+        //    File.AppendAllText(logPath, "[exception message]:" + condition + "\r\n"); File.AppendAllText(logPath, "[stack trace]:" + stackTrace + "\r\n");
+        //}
+        ////启动bug反馈程序
+        //if (File.Exists(BugExePath))
+        //{
+        //    ProcessStartInfo pros = new ProcessStartInfo();
+        //    pros.FileName = BugExePath;
+        //    pros.Arguments = "\"" + logPath + "\"";
+        //    Process pro = new Process();
+        //    pro.StartInfo = pros; pro.Start();
+        //}
+        ////退出程序，bug反馈程序重启主程序
+        //if (IsQuitWhenException)
+        //{
+        //    Application.Quit();
+        //}
 
     }
 }
