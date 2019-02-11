935a1ac46947b3d49dd051b414d45773a6243261
Allan <xuguangliangu01@126.com>
2019-02-11 18:01:11 +0800

    暂时注释掉编译时保存部分

--- Pro/C/U3D/TSG/Assets/Scripts/Manager/Tool/CSResourceManager.cs
+++ Pro/C/U3D/TSG/Assets/Scripts/Manager/Tool/CSResourceManager.cs
@@ -19,28 +19,28 @@ public class CSResourceManager : CSMonoSingleton<CSResourceManager>
 
         return go;
     }
-    public static T LoadAsset<T>(string path, string assetName) where T : UnityEngine.Object
-    {
-#if UNITY_EDITOR
-        //if (CSGame.Instance.IsLoadLocalRex)
-        //{
+    //public static T LoadAsset<T>(string path, string assetName) where T : UnityEngine.Object
+    //{
+//#if UNITY_EDITOR
+//        //if (CSGame.Instance.IsLoadLocalRex)
+//        //{
 
-            //UnityEngine.Debug.Log(path.ToLower() + assetName.ToLower() + "---" + assetName);
-            string[] assetPaths = UnityEditor.AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(path.ToLower() + assetName.ToLower(), assetName);
-            if (assetName.Length == 0)
-            {
-                if (Debug.developerConsoleVisible) Debug.LogError("There Is No Asset With Name \"" + assetName + "\"in " + assetName);
-                return null;
-            }
-            UnityEngine.Object target = UnityEditor.AssetDatabase.LoadMainAssetAtPath(assetPaths[0]);
-            return target as T;
-        //}
-#endif
+//            //UnityEngine.Debug.Log(path.ToLower() + assetName.ToLower() + "---" + assetName);
+//            string[] assetPaths = UnityEditor.AssetDatabase.GetAssetPathsFromAssetBundleAndAssetName(path.ToLower() + assetName.ToLower(), assetName);
+//            if (assetName.Length == 0)
+//            {
+//                if (Debug.developerConsoleVisible) Debug.LogError("There Is No Asset With Name \"" + assetName + "\"in " + assetName);
+//                return null;
+//            }
+//            UnityEngine.Object target = UnityEditor.AssetDatabase.LoadMainAssetAtPath(assetPaths[0]);
+//            return target as T;
+//        //}
+//#endif
 
         //LoadedAssetBundle assetBundle = AssetBundleManager.LoadUIAssetAsync(path.ToLower() + assetName.ToLower());
         //if (assetBundle == null)
         //    return null;
 
         //return assetBundle.m_AssetBundle.LoadAsset<T>(assetName);
-    }
+    //}
 }
