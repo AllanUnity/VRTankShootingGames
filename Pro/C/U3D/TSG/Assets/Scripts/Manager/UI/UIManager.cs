15ada8e7077e3acc9e17937030899f9244d1b8be
Allan <xuguangliangu01@126.com>
2019-02-11 16:37:23 +0800

    UIManager修改
    显示隐藏修改为字典记录, 并添加相关的添加和移除方法
    添加获取界面GetPanel方法
    添加获取隐藏界面GetHidePanel方法
    添加打开界面方法OpenPanel方法(未完成)
    添加回调

--- Pro/C/U3D/TSG/Assets/Scripts/Manager/UI/UIManager.cs
+++ Pro/C/U3D/TSG/Assets/Scripts/Manager/UI/UIManager.cs
@@ -1,44 +1,228 @@
-﻿using System;
+﻿using AssetBundles;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>UI管理类</summary>
-public class UIManager : MonoBehaviour
+public class UIManager : CSMonoSingleton<UIManager>
 {
+    #region List Operation
+    /// <summary>打开展示的界面</summary>
+    public Dictionary<Type, Dictionary<int, UIBase>> ShowPanelDictionary;
+    /// <summary>隐藏的面板</summary>
+    public Dictionary<Type, Dictionary<int, UIBase>> HidePanelDictionary;
 
-    private static UIManager instance;
-
-    public static UIManager Instance
+    private void AddShowPanel(UIBase ui, int id)
     {
-        get
+        if (ShowPanelDictionary != null)
         {
-            return instance;
+            Type type = ui.GetType();
+            Dictionary<int, UIBase> uiPanels;
+            if (ShowPanelDictionary.ContainsKey(type))
+            {
+                uiPanels = ShowPanelDictionary[type];
+                if (uiPanels != null)
+                {
+                    if (uiPanels.ContainsKey(id))
+                    {
+                        Debug.Log("UI已存在,ID重复" + type + id);
+                    }
+                }
+                else
+                {
+                    uiPanels = new Dictionary<int, UIBase>();
+                }
+
+            }
+            else
+            {
+                uiPanels = new Dictionary<int, UIBase>();
+                ShowPanelDictionary.Add(type, uiPanels);
+            }
+            uiPanels.Add(id, ui);
         }
     }
-
-    /// <summary>打开展示的界面</summary>
-    public List<UIBase> ShowPanels;
-    /// <summary>隐藏的面板</summary>
-    public List<UIBase> HidePanels;
-
-    private void Init()
+    private void RemoveShowPanel(UIBase ui, int id)
     {
-        ShowPanels = new List<UIBase>();
-        HidePanels = new List<UIBase>();
+        if (ShowPanelDictionary != null)
+        {
+            Type type = ui.GetType();
+            Dictionary<int, UIBase> uiPanels;
+            if (ShowPanelDictionary.TryGetValue(type, out uiPanels))
+            {
+                if (uiPanels != null)
+                {
+                    uiPanels.Remove(id);
+                }
+            }
+
+        }
+    }
+    private void AddHidePanel(UIBase ui, int id)
+    {
+        if (HidePanelDictionary != null)
+        {
+            Type type = ui.GetType();
+            Dictionary<int, UIBase> uiPanels;
+            if (HidePanelDictionary.ContainsKey(type))
+            {
+                uiPanels = HidePanelDictionary[type];
+                if (uiPanels != null)
+                {
+                    if (uiPanels.ContainsKey(id))
+                    {
+                        Debug.Log("UI已存在,ID重复" + type + id);
+                    }
+                }
+                else
+                {
+                    uiPanels = new Dictionary<int, UIBase>();
+                }
+
+            }
+            else
+            {
+                uiPanels = new Dictionary<int, UIBase>();
+                HidePanelDictionary.Add(type, uiPanels);
+            }
+            uiPanels.Add(id, ui);
+        }
+    }
+    private void RemoveHidePanel(UIBase ui, int id)
+    {
+        if (HidePanelDictionary != null)
+        {
+            Type type = ui.GetType();
+            Dictionary<int, UIBase> uiPanels;
+            if (HidePanelDictionary.TryGetValue(type, out uiPanels))
+            {
+                if (uiPanels != null)
+                {
+                    uiPanels.Remove(id);
+                }
+            }
+
+        }
+    }
+    #endregion
+    public override void Init()
+    {
+        base.Init();
+        ShowPanelDictionary = new Dictionary<Type, Dictionary<int, UIBase>>();
+        HidePanelDictionary = new Dictionary<Type, Dictionary<int, UIBase>>();
     }
 
-    public void OpenPanel()
-    {
 
-    }
-    public T GetPanel<T>() where T : UIBase
+    /// <summary>获取已打开的面板</summary>
+    /// <typeparam name="T">面板类型</typeparam>
+    /// <param name="uiid">id</param>
+    /// <returns></returns>
+    public T GetPanel<T>(int uiid = 0) where T : UIBase
     {
-
+        Type type = typeof(T);
+        if (ShowPanelDictionary != null)
+        {
+            Dictionary<int, UIBase> uiPanels;
+            if (ShowPanelDictionary.TryGetValue(type, out uiPanels))
+            {
+                if (uiPanels != null && uiPanels.Count > 0)
+                {
+                    UIBase ui;
+                    if (uiPanels.TryGetValue(uiid, out ui))
+                    {
+                        return ui as T;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Not Initialize UIManager");
+        }
         return null;
     }
-    public void ClosePanel()
+    public T GetHidePanel<T>(int uiid = 0) where T : UIBase
+    {
+        Type type = typeof(T);
+        if (HidePanelDictionary != null)
+        {
+            Dictionary<int, UIBase> uiPanels;
+            if (HidePanelDictionary.TryGetValue(type, out uiPanels))
+            {
+                if (uiPanels != null && uiPanels.Count > 0)
+                {
+                    UIBase ui;
+                    if (uiPanels.TryGetValue(uiid, out ui))
+                    {
+                        return ui as T;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Not Initialize UIManager");
+        }
+        return null;
+    }
+    /// <summary>界面是否打开</summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public bool IsOpenPanel<T>(int id = 0) where T : UIBase
+    {
+        T t = GetPanel<T>(id);
+        if (t != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>打开面板</summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="id"></param>
+    /// <param name="isHasAudio"></param>
+    /// <param name="cb"></param>
+    public void OpenPanel<T>(int id = 0, DelegateOpenPanelCallBack cb = null) where T : UIBase
+    {
+        UIBase ui = GetPanel<T>(id);
+        if (ui != null)
+        {
+            return;
+        }
+        ui = GetHidePanel<T>(id);
+        if (ui != null)
+        {
+            if (cb != null)
+            {
+                cb(ui);
+            }
+            RemoveHidePanel(ui, id);
+            AddShowPanel(ui, id);
+            return;
+        }
+        CSGame.Instance.StartCoroutine(LoadUIPanelPrefab(typeof(T), cb));
+    }
+
+    private IEnumerator LoadUIPanelPrefab(Type type, DelegateOpenPanelCallBack cb)
+    {
+        Entry entry = UIEnroll.Instance.FindEntry(type.Name);
+        if (entry == null || entry.path == null || string.IsNullOrEmpty(entry.path))
+        {
+            UnityEngine.Debug.LogError("界面注册错误 : " + type.Name);
+            yield break;
+        }
+        GameObject prefab = CSGame.Instance.GetStaticObj(entry.type.Name);
+
+
+    }
+    private void InstantiateUI(GameObject prefab)
     {
 
     }
 }
+/// <summary>打开面板回调</summary>
+/// <param name="ui"></param>
+public delegate void DelegateOpenPanelCallBack(UIBase ui);
