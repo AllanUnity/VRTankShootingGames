466544251f88d3103d6b316961f39684443d0c22
Allan <xuguangliangu01@126.com>
2019-02-11 16:31:01 +0800

    添加按键管理类

---
+++ Pro/C/U3D/TSG/Assets/Scripts/Manager/Input/InputManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 按键输入管理类
+/// </summary>
+public class InputManager : CSGameManagerBase<InputManager>
+{
+    KeyCode[] keys;
+    Dictionary<KeyCode, List<InputDelegate>> keysDelegate = new Dictionary<KeyCode, List<InputDelegate>>();
+    public static void Add(KeyCode key, InputDelegate callback)
+    {
+        Instance.AddCode(key, callback);
+    }
+    public static void Remove(KeyCode key)
+    {
+        Instance.RemoveCode(key);
+    }
+    public static void Remove(KeyCode key, InputDelegate callback)
+    {
+        Instance.RemoveCode(key, callback);
+    }
+    public override void Start()
+    {
+        base.Start();
+
+        string[] keyNames = Enum.GetNames(typeof(KeyCode));
+        int length = keyNames.Length;
+        keys = new KeyCode[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            keys[i] = (KeyCode)Enum.Parse(typeof(KeyCode), keyNames[i]);
+        }
+    }
+
+
+    /// <summary>
+    /// 添加
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="callback"></param>
+    public void AddCode(KeyCode key, InputDelegate callback)
+    {
+        List<InputDelegate> delegates = null;
+
+        if (!keysDelegate.ContainsKey(key))
+        {
+            delegates = new List<InputDelegate>();
+            keysDelegate.Add(key, delegates);
+        }
+        else
+        {
+            delegates = keysDelegate[key];
+        }
+        if (!delegates.Contains(callback))
+        {
+            delegates.Add(callback);
+        }
+
+    }
+    public void RemoveCode(KeyCode key)
+    {
+        keysDelegate.Remove(key);
+    }
+    /// <summary>
+    /// 移除事件
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="callback"></param>
+    public void RemoveCode(KeyCode key, InputDelegate callback)
+    {
+        if (keysDelegate.ContainsKey(key))
+        {
+            keysDelegate[key].Remove(callback);
+        }
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            for (int j = 0; j < keys.Length; j++)
+            {
+                KeyCode item = keys[j];
+                if (Input.GetKeyDown(item))
+                {
+                    if (keysDelegate != null && keysDelegate.ContainsKey(item) && keysDelegate[item] != null)
+                    {
+                        List<InputDelegate> delegates = keysDelegate[item];
+                        for (int i = 0; i < delegates.Count; i++)
+                        {
+                            delegates[i]();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+public delegate void InputDelegate();
