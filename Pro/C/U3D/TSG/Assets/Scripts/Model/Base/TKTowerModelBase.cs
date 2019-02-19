2d600cb553151060808eb9215b287fdcb74960f3
Allan <xuguangliangu01@126.com>
2019-02-19 18:14:21 +0800

    增加坦克控制器
    增加UI控制器
    坦克控制器增加自动旋转和增量旋转

--- Pro/C/U3D/TSG/Assets/Scripts/Model/Base/TKTowerModelBase.cs
+++ Pro/C/U3D/TSG/Assets/Scripts/Model/Base/TKTowerModelBase.cs
@@ -6,13 +6,61 @@ public class TKTowerModelBase : MonoBehaviour
 {
     public TKTowerControllerBase towerControllerBase;
 
-    public float towerTarget;
-    public float gunTarget;
+    /// <summary>炮塔的目标角度</summary>
+    public float towerTargetAngles;
+    /// <summary>炮管的目标角度</summary>
+    public float gunTargetAngles;
+
+    /// <summary>当前的炮塔的角度</summary>
+    private float currentTowerAngles;
+    /// <summary>当前的炮管的角度</summary>
+    private float currentGunAngles;
+
+    /// <summary>水平旋转速度</summary>
+    public float horizontalRotateSpeed;
+    /// <summary>竖直旋转速度</summary>
+    public float verticalRotateSpeed;
+
+    /// <summary>旋转方向</summary>
+    public RotateType rotateType;
 
     /// <summary>旋转炮塔</summary>
-    public void RotateTower()
+    public void RotateTower(float tower, float gun)
     {
-        
+        if (tower >= 180)
+        {
+            do
+            {
+                tower -= 180;
+            } while (tower >= 180);
+        }
+        else if (tower<=-180)
+        {
+            do
+            {
+                tower += 180;
+            } while (tower<=-180);
+        }
+        if (gun >= 180)
+        {
+            do
+            {
+                gun -= 180;
+            } while (gun >= 180);
+        }
+        else if (gun <= -180)
+        {
+            do
+            {
+                gun += 180;
+            } while (gun <= -180);
+        }
+        towerTargetAngles = tower;
+        gunTargetAngles = gun;
+    }
+    public void RotateTower(RotateType type)
+    {
+        rotateType = type;
     }
 
     public void Update()
@@ -21,6 +69,50 @@ public class TKTowerModelBase : MonoBehaviour
     }
     public virtual void OnUpdate()
     {
-        towerControllerBase.SetTargetVector(towerTarget, gunTarget);
+        ChangeTargetAngles();
+        RotateCurrentTowerAndGun();
     }
+
+    public virtual void ChangeTargetAngles()
+    {
+        switch (rotateType)
+        {
+            case RotateType.Left:
+                towerTargetAngles += (Time.deltaTime*horizontalRotateSpeed);
+                break;
+            case RotateType.Right:
+                towerTargetAngles -= (Time.deltaTime * horizontalRotateSpeed);
+                break;
+        }
+    }
+    /// <summary>旋转炮塔和炮管</summary>
+    public virtual void RotateCurrentTowerAndGun()
+    {
+        if (currentTowerAngles < towerTargetAngles)
+        {
+            currentTowerAngles = currentTowerAngles + (horizontalRotateSpeed * Time.deltaTime);
+        }
+        if (currentTowerAngles > towerTargetAngles)
+        {
+            currentTowerAngles = currentTowerAngles - (horizontalRotateSpeed * Time.deltaTime);
+        }
+
+        if (currentGunAngles < gunTargetAngles)
+        {
+            currentGunAngles = currentGunAngles + (verticalRotateSpeed * Time.deltaTime);
+        }
+        if (currentGunAngles > gunTargetAngles)
+        {
+            currentGunAngles = currentGunAngles - (verticalRotateSpeed * Time.deltaTime);
+        }
+
+        towerControllerBase.SetTargetVector(currentTowerAngles, currentGunAngles);
+    }
+}
+
+public enum RotateType
+{
+    Left,
+    Stop,
+    Right
 }
---
+++ Pro/C/U3D/TSG/Assets/Scripts/UI/view/UITestControllerTower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITestControllerTower : MonoBehaviour
+{
+    public TKTowerModelBase tower;
+    public InputField towerInputField;
+    public InputField gunInputField;
+    public Button countersignBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countersignBtn.onClick.AddListener(() =>
+        {
+            float towerF = float.Parse(towerInputField.text);
+            float gunF = float.Parse(gunInputField.text);
+
+            tower.RotateTower(towerF, gunF);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
