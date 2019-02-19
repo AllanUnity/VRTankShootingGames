dfdc23349f661ac44dd24c9867dc4e3cd84b3249
Allan <xuguangliangu01@126.com>
2019-02-19 12:47:58 +0800

    MVC模式和Test场景

---
+++ Pro/C/U3D/TSG/Assets/Scripts/Model/Base/TKTowerModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TKTowerModelBase : MonoBehaviour
+{
+    public TKTowerControllerBase towerControllerBase;
+
+    public float towerTarget;
+    public float gunTarget;
+
+    /// <summary>旋转炮塔</summary>
+    public void RotateTower()
+    {
+        
+    }
+
+    public void Update()
+    {
+        OnUpdate();
+    }
+    public virtual void OnUpdate()
+    {
+        towerControllerBase.SetTargetVector(towerTarget, gunTarget);
+    }
+}
