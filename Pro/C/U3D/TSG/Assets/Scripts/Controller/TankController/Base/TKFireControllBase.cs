2b33e7361403fd018a2f6c2597834f83eed7dcdb
Allan <xuguangliangu01@126.com>
2019-02-20 18:12:32 +0800

    加入开炮代码

---
+++ Pro/C/U3D/TSG/Assets/Scripts/Controller/TankController/Base/TKFireControllBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TKFireControllBase : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public GameObject firePosition;
+
+    public void Fire()
+    {
+        GameObject _bullet = Instantiate(bulletPrefab, firePosition.transform.position, firePosition.transform.rotation);
+        Rigidbody _rigidbody = _bullet.AddComponent<Rigidbody>();
+        //_rigidbody.AddRelativeForce(Vector3.forward*10, ForceMode.Impulse);
+    }
+
+
+}
