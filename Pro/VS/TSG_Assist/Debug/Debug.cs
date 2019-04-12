ee6fbcf15ea793e24c6c283339431d757132015a
Allan <xuguangliangu01@126.com>
2019-04-12 18:10:23 +0800

    ....Debug

---
+++ Pro/VS/TSG_Assist/Debug/Debug.cs
@@ -0,0 +1,27 @@
+﻿
+public class Debug
+{
+    public static bool developerConsoleVisible;
+    public static void Log(object obj)
+    {
+        UnityEngine.Debug.Log(obj);
+    }
+    public static void LogColor(object obj)
+    {
+        string str = "<color=yellow>" + obj.ToString() + "</color>";
+        Log(str);
+    }
+    public static void LogError(object obj)
+    {
+        UnityEngine.Debug.LogError(obj);
+    }
+    public static void LogWarning(object obj)
+    {
+        UnityEngine.Debug.LogWarning(obj);
+    }
+    public static void LogException(System.Exception obj)
+    {
+        UnityEngine.Debug.LogException(obj);
+    }
+}
+
